ff8ded8ea910437034123c5629705527aacbfc56
Paritosh Barman <barmanpari163@gmail.com>
2023-10-24 00:09:20 +0530

    Fixd the prime code

--- Prime Problem.js
+++ Prime Problem.js
@@ -1,3 +1,13 @@
-let name = paritosh
+let num = 20;
 
-if (0%==2)
+let sum = 0;
+for(let i=0; i<num; i++){
+    if(num%i==0){
+        sum++
+    }
+}
+if(sum==2){
+    console.log("Prime");
+}else{
+    console.log("Not Prime");
+}
